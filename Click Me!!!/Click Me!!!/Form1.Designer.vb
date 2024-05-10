bdbdbcb1282c85a920bdba903a1512454f81e0d3
QuestDragon <126067454+QuestDragon@users.noreply.github.com>
2024-05-11 01:14:17 +0900

    Click Me!! アップロード
    Click Me!!ファイルをアップロードしました。

---
+++ Click Me!!!/Click Me!!!/Form1.Designer.vb
@@ -0,0 +1,144 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("小塚ゴシック Pro R", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label1.Location = New System.Drawing.Point(25, 29)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(405, 108)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Click Me!!!"
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label2.Font = New System.Drawing.Font("DS-Digital", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(148, 136)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(185, 79)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "0"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.Font = New System.Drawing.Font("小塚ゴシック Pro R", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(32, 249)
+        Me.ComboBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(179, 54)
+        Me.ComboBox1.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Yu Gothic UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Button1.Location = New System.Drawing.Point(233, 249)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(203, 54)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("小塚ゴシック Pro B", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.Red
+        Me.Button2.Location = New System.Drawing.Point(32, 401)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(400, 144)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Attack"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("メイリオ", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Button3.Location = New System.Drawing.Point(37, 568)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(393, 45)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "End"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("DS-Digital", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(137, 322)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(193, 59)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "left: -"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(475, 629)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Click Me!!!"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Click Me!!!/Click Me!!!/Form1.vb
@@ -0,0 +1,117 @@
+﻿Option Explicit On
+Public Class Form1
+    Dim A As Integer
+    Dim B As Integer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        A = 0
+        ComboBox1.Items.Add("10秒")
+        ComboBox1.Items.Add("30秒")
+        ComboBox1.Items.Add("60秒")
+        ComboBox1.Items.Add("未設定")
+        Me.FormBorderStyle = FormBorderStyle.FixedSingle
+        ComboBox1.SelectedIndex = 3
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        A = A + 1
+        Label2.Text = A
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim Ended As Integer
+        Ended = MsgBox("終了しますか？", 32 + 4 + vbDefaultButton2, "Click Me!!!")
+        Select Case Ended '押されたボタンの確認
+            Case vbYes
+                End
+        End Select
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label2.Text = 0
+        A = 0
+        If ComboBox1.SelectedIndex <> 3 Then 'タイマーを開始した場合、Startボタンを無効化
+            Button1.Enabled = False
+        End If
+        Button2.Enabled = True 'Attackボタンの有効化
+        Button2.Focus()
+        If ComboBox1.SelectedIndex = 0 Then
+            Call count3()
+        ElseIf Combobox1.SelectedIndex = 1 Then
+            Call count2()
+        ElseIf ComboBox1.SelectedIndex = 2 Then
+            Call count()
+        Else
+            Button2.Enabled = True
+        End If
+    End Sub
+    Sub count()
+        Dim t As Long
+        Dim endTime As Date
+        Dim rtn As Integer
+        t = 60
+        endTime = DateAdd("s", t, Now)
+        Do Until t <= 0
+            t = DateDiff("s", Now, endTime)
+            Label3.Text = " left: " & t
+            My.Application.DoEvents()
+        Loop
+        rtn = MsgBox("時間切れ！！", 16, "Click Me!!!")
+        Select Case rtn '押されたボタンの確認
+            Case vbOK
+                Button2.Enabled = False
+                Label3.Text = "left: -"
+                Button1.Enabled = True
+        End Select
+    End Sub
+    Sub count2()
+        Dim t As Long
+        Dim endTime As Date
+        Dim rtn As Integer
+        t = 30
+        endTime = DateAdd("s", t, Now)
+        Do Until t <= 0
+            t = DateDiff("s", Now, endTime)
+            Label3.Text = " left: " & t
+            My.Application.DoEvents()
+        Loop
+        rtn = MsgBox("時間切れ！！", 16, "Click Me!!!")
+        Select Case rtn '押されたボタンの確認
+            Case vbOK
+                Button2.Enabled = False
+                Label3.Text = "left: -"
+                Button1.Enabled = True
+        End Select
+    End Sub
+    Sub count3()
+        Dim t As Long
+        Dim endTime As Date
+        Dim rtn As Integer
+        t = 10
+        endTime = DateAdd("s", t, Now)
+        Do Until t <= 0
+            t = DateDiff("s", Now, endTime)
+            Label3.Text = " left: " & t
+            My.Application.DoEvents()
+        Loop
+        rtn = MsgBox("時間切れ！！", 16, "Click Me!!!")
+        Select Case rtn '押されたボタンの確認
+            Case vbOK
+                Button2.Enabled = False
+                Label3.Text = "left: -"
+                Button1.Enabled = True
+        End Select
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        If ComboBox1.SelectedIndex = 3 Then
+            Button1.Text = "Reset"
+        Else
+            Button1.Text = "Start"
+        End If
+    End Sub
+    Private Sub Form1_Closing(ByVal sender As Object, ByVal e As System.ComponentModel.CancelEventArgs) Handles MyBase.Closing
+        Dim confirm As Integer = MsgBox("終了しますか？", 32 + 4 + vbDefaultButton2, "Click Me!!!")
+        If confirm = vbNo Then
+            e.Cancel = True
+        End If
+    End Sub
+End Class
