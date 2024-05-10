641f90f18dc5ac6858bd89e94dfeffafdda095a7
QuestDragon <126067454+QuestDragon@users.noreply.github.com>
2024-05-11 01:16:40 +0900

    CLICK ME For C Sharp アップロード
    Click Me!! C#版のファイルをアップロードしました。

---
+++ CLICK ME For C Sharp/CLICK ME For C Sharp/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace CLICK_ME_For_C_Sharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.End = new System.Windows.Forms.Button();
+            this.Title = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("游ゴシック", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(326, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 68);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Click Me";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("游ゴシック Medium", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(213, 24);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(88, 42);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "0";
+            // 
+            // End
+            // 
+            this.End.Font = new System.Drawing.Font("游ゴシック", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.End.Location = new System.Drawing.Point(326, 109);
+            this.End.Name = "End";
+            this.End.Size = new System.Drawing.Size(200, 68);
+            this.End.TabIndex = 0;
+            this.End.Text = "End";
+            this.End.UseVisualStyleBackColor = true;
+            this.End.Click += new System.EventHandler(this.End_Click);
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("Meiryo UI", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Title.Location = new System.Drawing.Point(12, 24);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(195, 47);
+            this.Title.TabIndex = 2;
+            this.Title.Text = "Click Me!!";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Meiryo UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(14, 115);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(106, 43);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("游ゴシック Medium", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(213, 116);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(88, 42);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "0";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("游ゴシック", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button2.Location = new System.Drawing.Point(126, 115);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(81, 43);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Adobe Gothic Std B", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(33, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(148, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "For C Sharp Edition";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 194);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Title);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.End);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Click Me!! For C Sharp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button End;
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ CLICK ME For C Sharp/CLICK ME For C Sharp/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CLICK_ME_For_C_Sharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.Add("未設定");
+            comboBox1.Items.Add("10秒");
+            comboBox1.Items.Add("30秒");
+            comboBox1.Items.Add("60秒");
+            comboBox1.SelectedIndex = 0;
+        }
+
+        int A = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            A = A + 1;
+            label1.Text = A.ToString();
+        }
+
+        private void End_Click(object sender, EventArgs e)
+        {
+            DialogResult exit = MessageBox.Show("終了しますか？",  "Click Me!! For C Sharp",MessageBoxButtons.YesNo , MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button2);
+            if ( exit == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            A = 0;
+            label1.Text = A.ToString();
+            button1.Enabled = true;
+            button1.Focus();
+            if (comboBox1.SelectedIndex == 0)
+            {
+                A = 0;
+                label2.Text = "0s";
+                button1.Enabled = true;
+                button1.Focus();
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                button2.Enabled = false;
+                button1.Focus();
+                count3();
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                button2.Enabled = false;
+                button1.Focus();
+                count2();
+            }
+            else if (comboBox1.SelectedIndex == 3)
+            {
+                button2.Enabled = false;
+                button1.Focus();
+                count();
+            }
+        }
+
+        private int counter;
+        private string s = "s";
+
+        private void count()
+        {
+            counter = 60;
+            timer1 = new Timer();
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Interval = 1000; // 1 second
+            timer1.Start();
+            label2.Text = counter.ToString() + s;
+        }
+
+        private void count2()
+        {
+            counter = 30;
+            timer1 = new Timer();
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Interval = 1000; // 1 second
+            timer1.Start();
+            label2.Text = counter.ToString() + s;
+
+        }
+
+        private void count3()
+        {
+            counter = 10;
+            timer1 = new Timer();
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Interval = 1000; // 1 second
+            timer1.Start();
+            label2.Text = counter.ToString() + s;
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            counter = counter - 1;
+            label2.Text = counter.ToString() + s;
+            if (counter == 0)
+            {
+                timer1.Stop();
+                label2.Text = counter.ToString() + s;
+                DialogResult timeup = MessageBox.Show("時間切れ！！", "Click Me!! For C Sharp", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                if (timeup == DialogResult.OK)
+                {
+                    button1.Enabled = false;
+                    button2.Enabled = true;
+                    label2.Text = "--";
+                }
+            }
+        }
+    }
+}
